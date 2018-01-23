0d5670b685fc88e3361b2402354ae8d02d832fe7
Cody Short <nccgs117@gmail.com>
2018-01-23 20:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object, {[key]:value})
   return newObject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]:value})
+}
